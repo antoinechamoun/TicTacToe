bc0c503646576ea29fd4899b3a102aaf0f738026
Tony <tonychamoun6@gmail.com>
2022-12-07 22:17:24 +0200

    Finish TicTacToe

--- javascript.js
+++ javascript.js
@@ -5,6 +5,8 @@ const gameBoard = (function GameBoard(){
     let winner = ''
     let restartBtn = document.querySelector('.btn')
     let versusChoice = document.getElementById('versus')
+    let compChoice 
+    let cellSelected
 
     // Add event listener
     window.addEventListener('click', play)
@@ -24,7 +26,7 @@ const gameBoard = (function GameBoard(){
             let idIndex = e.target.className
             let index = idIndex.split(" ")[1];
             
-            let cellSelected = document.getElementById(`${choice}`)
+            cellSelected = document.getElementById(`${choice}`)
             
             if(cellSelected.textContent !== ''){
                 alert('Please select an empty cell')
@@ -38,11 +40,13 @@ const gameBoard = (function GameBoard(){
                 isX=!isX
                 if(versusChoice.value!=='1vs1'){
                     checkWin()
+                    console.log('checked 2' + winner);
                     if(winner!==''){
                         restart()
                         return
                     }
                     computerPlay()
+                    console.log('chosen');
                 }
             }
             checkWin()
@@ -54,6 +58,7 @@ const gameBoard = (function GameBoard(){
 
     // Check if there is a winner
     function checkWin(){
+        console.log('check is win?');
         if(gameBoardArray[0] === gameBoardArray[1] && gameBoardArray[1] === gameBoardArray[2] && gameBoardArray[1] !== ''||
         gameBoardArray[0] === gameBoardArray[4] && gameBoardArray[4] === gameBoardArray[8] && gameBoardArray[4] !== ''||
         gameBoardArray[0] === gameBoardArray[3] && gameBoardArray[3] === gameBoardArray[6] && gameBoardArray[3] !== ''){
@@ -75,6 +80,7 @@ const gameBoard = (function GameBoard(){
                 alert(winner + " is the winner")
             }
         }
+        console.log('checked 1');
 
     }
 
@@ -91,60 +97,258 @@ const gameBoard = (function GameBoard(){
 
     // Computer play function
     function computerPlay(){
+        console.log('enter pc');
         let computerLevel = versusChoice.value.split("_")[1];
-        let compChoice 
         do{
             compChoice = Math.floor(Math.random()*9)
             cellSelected = document.getElementById(`${choiceStr[compChoice]}`)
         }while(cellSelected.textContent!=='')
-        console.log(cellSelected);
+        console.log('choose first');
         if(computerLevel === 'medium'){
-            if(checkLine(0,1,2)){
-                compChoice=2
-            }else if(checkLine(0,2,1)){
-                compChoice=1
-            }else if(checkLine(1,2,0)){
-                compChoice=0
-            }else if(checkLine(3,4,5)){
-                compChoice=5
-            }else if(checkLine(3,5,4)){
-                compChoice=4
-            }else if(checkLine(4,5,3)){
-                compChoice=3
-            }else if(checkLine(6,7,8)){
-                compChoice=8
-            }else if(checkLine(6,8,7)){
-                compChoice=7
-            }else if(checkLine(7,8,6)){
-                compChoice=6
-            }else if(checkLine(0,4,8)){
-                compChoice=8
-            }else if(checkLine(0,8,4)){
-                compChoice=4
-            }else if(checkLine(4,8,0)){
-                compChoice=0
-            }else if(checkLine(2,4,6)){
-                compChoice=6
-            }else if(checkLine(2,6,4)){
-                compChoice=4
-            }else if(checkLine(6,4,2)){
-                compChoice=2
-            }
+            checkMedium()
+            console.log('choose medium');
         }else{
-            
+            console.log('first: '+compChoice);
+            if(!checkAllWin()){
+                console.log('second: '+compChoice);
+                if(!checkMedium()){
+                    console.log('third: '+compChoice);
+                    checkCorners()
+                    console.log('fourth: '+compChoice);
+                }
+            }
         }
+        
+        cellSelected = document.getElementById(`${choiceStr[compChoice]}`)
         console.log(cellSelected);
         cellSelected.textContent='O';
         gameBoardArray[compChoice]='O';
         isX=true
     }
 
-    function checkLine(ind0, ind1, ind2){
+    // function that blocks player
+    function checkLineBlock(ind0, ind1, ind2){
         if(gameBoardArray[ind0]===gameBoardArray[ind1] && gameBoardArray[ind1]!=='' && gameBoardArray[ind2]=== ''){
             cellSelected = document.getElementById(choiceStr[ind2])
             compChoice = ind2
             return true
         }
         return false
+    }
+
+    // function checks medium choice
+    function checkMedium(){
+        let result=false
+        if(checkLineBlock(0,1,2)){
+            compChoice=2
+            result=true
+        }else if(checkLineBlock(0,2,1)){
+            compChoice=1
+            result=true
+        }else if(checkLineBlock(1,2,0)){
+            compChoice=0
+            result=true
+        }else if(checkLineBlock(3,4,5)){
+            compChoice=5
+            result=true
+        }else if(checkLineBlock(3,5,4)){
+            compChoice=4
+            result=true
+        }else if(checkLineBlock(4,5,3)){
+            compChoice=3
+            result=true
+        }else if(checkLineBlock(6,7,8)){
+            compChoice=8
+            result=true
+        }else if(checkLineBlock(6,8,7)){
+            compChoice=7
+            result=true
+        }else if(checkLineBlock(7,8,6)){
+            compChoice=6
+            result=true
+        }else if(checkLineBlock(0,4,8)){
+            compChoice=8
+            result=true
+        }else if(checkLineBlock(0,8,4)){
+            compChoice=4
+            result=true
+        }else if(checkLineBlock(4,8,0)){
+            compChoice=0
+            result=true
+        }else if(checkLineBlock(2,4,6)){
+            compChoice=6
+            result=true
+        }else if(checkLineBlock(2,6,4)){
+            compChoice=4
+            result=true
+        }else if(checkLineBlock(6,4,2)){
+            compChoice=2
+            result=true
+        }else if(checkLineBlock(0,3,6)){
+            compChoice=6
+            result=true
+        }else if(checkLineBlock(0,6,3)){
+            compChoice=3
+            result=true
+        }else if(checkLineBlock(6,3,0)){
+            compChoice=0
+            result=true
+        }else if(checkLineBlock(1,4,7)){
+            compChoice=7
+            result=true
+        }else if(checkLineBlock(1,7,4)){
+            compChoice=4
+            result=true
+        }else if(checkLineBlock(4,7,1)){
+            compChoice=1
+            result=true
+        }else if(checkLineBlock(2,5,8)){
+            compChoice=8
+            result=true
+        }else if(checkLineBlock(5,8,2)){
+            compChoice=2
+            result=true
+        }else if(checkLineBlock(2,8,5)){
+            compChoice=5
+            result=true
+        }
+        return result
+    }
+
+    // function checks all winning line
+    function checkAllWin(){
+        let result = false
+        if(checkLineWin(0,1,2)){
+            compChoice=2
+            result=true
+        }else if(checkLineWin(0,2,1)){
+            compChoice=1
+            result=true
+        }else if(checkLineWin(1,2,0)){
+            compChoice=0
+            result=true
+        }else if(checkLineWin(3,4,5)){
+            compChoice=5
+            result=true
+        }else if(checkLineWin(3,5,4)){
+            compChoice=4
+            result=true
+        }else if(checkLineWin(4,5,3)){
+            compChoice=3
+            result=true
+        }else if(checkLineWin(6,7,8)){
+            compChoice=8
+            result=true
+        }else if(checkLineWin(6,8,7)){
+            compChoice=7
+            result=true
+        }else if(checkLineWin(7,8,6)){
+            compChoice=6
+            result=true
+        }else if(checkLineWin(0,4,8)){
+            compChoice=8
+            result=true
+        }else if(checkLineWin(0,8,4)){
+            compChoice=4
+            result=true
+        }else if(checkLineWin(4,8,0)){
+            compChoice=0
+            result=true
+        }else if(checkLineWin(2,4,6)){
+            compChoice=6
+            result=true
+        }else if(checkLineWin(2,6,4)){
+            compChoice=4
+            result=true
+        }else if(checkLineWin(6,4,2)){
+            compChoice=2
+            result=true
+        }else if(checkLineWin(0,3,6)){
+            compChoice=6
+            result=true
+        }else if(checkLineWin(0,6,3)){
+            compChoice=3
+            result=true
+        }else if(checkLineWin(6,3,0)){
+            compChoice=0
+            result=true
+        }else if(checkLineWin(1,4,7)){
+            compChoice=7
+            result=true
+        }else if(checkLineWin(1,7,4)){
+            compChoice=4
+            result=true
+        }else if(checkLineWin(4,7,1)){
+            compChoice=1
+            result=true
+        }else if(checkLineWin(2,5,8)){
+            compChoice=8
+            result=true
+        }else if(checkLineWin(5,8,2)){
+            compChoice=2
+            result=true
+        }else if(checkLineWin(2,8,5)){
+            compChoice=5
+            result=true
+        }
+        return result
+    }
+
+    // function checks for winning
+    function checkLineWin(ind0, ind1, ind2){
+        if(gameBoardArray[ind0]===gameBoardArray[ind1] && gameBoardArray[ind1]==='O' && gameBoardArray[ind2]=== ''){
+            cellSelected = document.getElementById(choiceStr[ind2])
+            compChoice = ind2
+            return true
+        }
+        return false
+    }
+
+    // function check for empty corners
+    function checkCorners(){
+        let result = false
+        if(gameBoardArray[4]===''){
+            compChoice=4
+            result=true
+        }else if(gameBoardArray[0] === 'X' && gameBoardArray[8]===''){
+            compChoice=8
+            result=true
+        }else if(gameBoardArray[2]==='X' && gameBoardArray[6]===''){
+            compChoice=6
+            result=true
+        }else if(gameBoardArray[6]==='X' && gameBoardArray[2]===''){
+            compChoice=2
+            result=true
+        }else if(gameBoardArray[8]==='X' && gameBoardArray[0]===''){
+            compChoice=0
+            result=true
+        }else if(gameBoardArray[0] === gameBoardArray[8] && gameBoardArray[0]==='X'|| gameBoardArray[2] === gameBoardArray[6] && gameBoardArray[2]==='X'){
+            if(gameBoardArray[1] === ''){
+                compChoice=1
+                result=true
+            }else if(gameBoardArray[3] === ''){
+                compChoice=3
+                result=true
+            }else if(gameBoardArray[7] === ''){
+                compChoice=7
+                result=true
+            }else if(gameBoardArray[5] === ''){
+                compChoice=5
+                result=true
+            }
+        }else if(gameBoardArray[0]===''){
+            compChoice=0
+            result=true
+        }else if(gameBoardArray[6]===''){
+            compChoice=6
+            result=true
+        }else if(gameBoardArray[8]===''){
+            compChoice=8
+            result=true
+        }else if(gameBoardArray[2]===''){
+            compChoice=0
+            result=true
+        }
+        return result
     }
 })()
